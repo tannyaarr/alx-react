5467b4da20544dbc81893ef3844896101acc896c
tannyaarr <tanyachiras@gmail.com>
2024-05-21 23:10:11 +0200

    changes to file

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,30 +1,18 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
-import logo from '../assets/holberton-logo.jpg';
 
-$(document).ready(function() {
-  $('body').append('<div id="logo"></div>');
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="start-btn">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-  $('#logo').css({
-    'width': '200px',
-    'height': '200px',
-    'background-image': `url(${logo})`,
-    'background-size': '200px 200px',
-    'background-repeat': 'no-repeat',
-  });
+let count = 0;
 
-  let count = 0;
-
-  function updateCounter() {
+function updateCounter() {
     count++;
     $('#count').text(`${count} clicks on the button`);
-  }
+}
 
-  $('#start-btn').on('click', _.debounce(updateCounter, 1000, { leading: true, trailing: false }));
-});
+$('button').on('click', _.debounce(updateCounter, 500));
